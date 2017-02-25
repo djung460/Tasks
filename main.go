e46773cb430232fc8a364e7d11195a77ee480ddf
David Jung <d.jung460@gmail.com>
2017-02-25 11:17:43 -0800

    Add types for this project

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/djung460/Tasks/views"
+)
+
+func main() {
+	views.PopulateTemplate()
+
+	http.Handle("/static/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/completed/", views.RequiresLogin(views.ShowCompletedTasksFunc))
+	http.HandleFunc("/complete/", views.CompleteTaskFunc)
+	http.HandleFunc("/add/", views.AddTaskFunc)
+	http.HandleFunc("/login/", views.LoginFunc)
+	http.HandleFunc("/logout/", views.LogoutFunc)
+	http.HandleFunc("/", views.RequiresLogin(views.HomeFunc))
+	fmt.Println("Server running on 8081")
+	err := http.ListenAndServe("0.0.0.0:8081", nil)
+	if err != nil {
+		fmt.Println("main.go: ", err)
+	}
+}
---
+++ types/types.go
@@ -0,0 +1,37 @@
+package types
+
+import "html/template"
+
+type Category struct {
+	Name      string
+	ID        string
+	TaskCount string
+}
+
+type Comment struct {
+	ID      string
+	Content string
+	Author  string
+	Created string
+}
+
+type Task struct {
+	ID          string
+	Title       string
+	Content     string
+	HTMLContent template.HTML
+	Priority    string
+	Category    string
+	Hidden      string
+	Created     string
+	Comments    []Comment
+}
+
+type Context struct {
+	Tasks      []Task
+	CSRFToken  string
+	Categories []Category
+}
+
+type Comments []Comment
+type Tasks []Task
